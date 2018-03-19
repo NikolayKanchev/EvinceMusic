04d0f71fe8424e788d0f8edc78b12c6b943e16b0
Nikolay Kanchev <nikolaj.kanchev@gmail.com>
2018-03-19 10:24:57 +0100

    The validation of the fields is working very nice in the Angular way (For Login, Register, ResetPassword components)

--- angular-nodejs-app/src/app/register/register.component.ts
+++ angular-nodejs-app/src/app/register/register.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-register',
@@ -6,10 +8,36 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./register.component.scss']
 })
 export class RegisterComponent implements OnInit {
+  private registerForm: FormGroup;
+  // emailPlaceholder: string = "E-mail";
 
-  constructor() { }
+  // constructor(private fb: FormBuilder, private authService: AuthService, private router: Router) { }
+  constructor(private fb: FormBuilder, private router: Router) { }
 
-  ngOnInit() {
+  onSubmitLogin(registerForm){
+    // if(loginForm.valid){
+    //   console.log("Form valid: ", loginForm.valid);
+    //   this.authService.login().subscribe(() => {
+    //     console.log("Now I am logged in!");
+    //     this.router.navigateByUrl(this.authService.redirectUrl);
+    //   })
+    // }else{
+    //   console.log("Form valid: ", loginForm.valid);
+    // }
   }
 
+  ngOnInit() {
+    this.createForm();
+  }
+
+  createForm(): any {
+    this.registerForm = this.fb.group({
+      firstName: ["", Validators.required],
+      lastName: ["", Validators.required],
+      username: ["", Validators.required],
+      email: ["", [Validators.required, Validators.email]],
+      password: ["", Validators.required],
+      retypePassword: ["", Validators.required]
+    })
+  }
 }
