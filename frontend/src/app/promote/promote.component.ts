dc2064e2dd994417475722551df1c8d4af5bc32e
NikolayKanchev <nikolaj.kanchev@gmail.com>
2018-03-28 00:36:59 +0200

    Promote page done

--- frontend/src/app/promote/promote.component.ts
+++ frontend/src/app/promote/promote.component.ts
@@ -1,15 +1,34 @@
 import { Component, OnInit } from '@angular/core';
+import { trigger, state, style, transition, animate, keyframes} from '@angular/animations'
+
 
 @Component({
   selector: 'app-promote',
   templateUrl: './promote.component.html',
-  styleUrls: ['./promote.component.scss']
+  styleUrls: ['./promote.component.scss'],
+  animations: [
+    trigger('popOverState', [
+      state('show', style({
+        opacity: 1
+      })),
+      state('hide',   style({
+        opacity: 0
+      })),
+      transition('show => hide', animate('600ms ease-out')),
+      transition('hide => show', animate('1000ms ease-in'))
+    ])
+  ]
 })
 export class PromoteComponent implements OnInit {
+  show = false;
 
   constructor() { }
 
-  ngOnInit() {
+  get stateName() {
+    return this.show ? 'show' : 'hide'
   }
 
+  ngOnInit() {
+    setTimeout(() => this.show = true, 1000);
+  }
 }
