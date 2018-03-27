72e0b57319457653c8b0c754395e5093c9d1a13f
NikolayKanchev <nikolaj.kanchev@gmail.com>
2018-03-27 23:15:16 +0200

    Home page done

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,21 +3,22 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import { Material } from './material';
+import { AuthService } from './auth.service';
+import { AuthGuard } from './auth.guard';
+import { DataService } from './data.service';
+
 import { LoginComponent } from './login/login.component';
 import { HomeComponent } from './home/home.component';
 import { GlobalpartnershipComponent } from './globalpartnership/globalpartnership.component';
 import { PromoteComponent } from './promote/promote.component';
 import { RegisterComponent } from './register/register.component';
-import { Material } from './material';
 import { ProfessionalServicesComponent } from './professional-services/professional-services.component';
 import { TalentComponent } from './talent/talent.component';
 import { ContactsComponent } from './contacts/contacts.component';
 import { ResetpasswordComponent } from './resetpassword/resetpassword.component';
 import { ReactiveFormsModule, FormsModule } from '@angular/forms';
 import {HttpClientModule} from '@angular/common/http';
-import { AuthService } from './auth.service';
-import { AuthGuard } from './auth.guard';
-import { DataService } from './data.service';
 import { AdminpageComponent } from './adminpage/adminpage.component';
 import { AccSettingsComponent } from './acc-settings/acc-settings.component';
 
