71b00a843159e12076ccbe226471a83a99a00e7a
NikolayKanchev <nikolaj.kanchev@gmail.com>
2018-04-06 13:03:48 +0200

    The users table in the admin-page is working

--- frontend/src/app/adminpage/users/users.component.ts
+++ frontend/src/app/adminpage/users/users.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { User } from '../../entities/user';
 import { DataService } from '../../data.service';
+import { MatTableDataSource } from '@angular/material';
+import { Auth1Service } from '../../auth1.service';
 
 @Component({
   selector: 'app-users',
@@ -10,11 +12,19 @@ import { DataService } from '../../data.service';
 export class UsersComponent implements OnInit {
   private users: User[];
   user: User;
+
+  displayedColumns = ['firstName', 'lastName', 'username', 'email'];
+  dataSource = new MatTableDataSource();
   
-  constructor(private ds: DataService) { }
+  constructor(private ds: DataService, private authService: Auth1Service) { }
 
   ngOnInit() {
     this.users = this.ds.getUsers();
+    this.authService.getUsers().subscribe(
+      data => {
+        this.dataSource.data = data;
+      }
+    );
   }
 
   onUserClicked(obj){
